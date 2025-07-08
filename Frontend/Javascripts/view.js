43a2ee1d1ac5eeed5228e11ff46a86bc60d8b468
aliserhan315 <aliserhan315@gmail.com>
2025-07-08 15:05:37 +0300

    testing the fetch problem in view

--- Frontend/Javascripts/view.js
+++ Frontend/Javascripts/view.js
@@ -9,33 +9,37 @@ const filmId = params.get("title");
 document.getElementById("bookBtn").onclick = () => {
   window.location.href = "../pages/seat.html";
 };
+const res = await fetchFilms(); 
+const films = res.data;           
 
-(async () => {
-  try {
-    const res = await fetchFilms();
-    const films = res.films;
+// (async () => {
+//   try {
+//     const res = await fetchFilms();
+//     console.log("Fetched Films:", res);
 
-    const selectedFilm = films.find(f => f.title == filmId);
-    if (!selectedFilm) {
-      console.error("Film not found");
-      return;
-    }
+//     const films = res.data;
 
-    console.log("Selected Film:", selectedFilm);
+//     const selectedFilm = films.find(f => f.title == filmId);
+//     if (!selectedFilm) {
+//       console.error("Film not found");
+//       return;
+//     }
 
-    document.querySelector(".detail-container").style.backgroundImage = `url('${selectedFilm.background_image}')`;
-    document.getElementById("poster").src = selectedFilm.poster_image;
-    document.getElementById("title").textContent = selectedFilm.title;
-    document.getElementById("description").textContent = selectedFilm.description || '';
-    document.getElementById("releaseDate").textContent = `Release Date: ${selectedFilm.release_date}`;
-    document.getElementById("rating").textContent = `Rating: ${selectedFilm.rating} stars`;
-    document.getElementById("duration").textContent = `Duration: ${selectedFilm.duration} minutes`;
+//     console.log("Selected Film:", selectedFilm);
+
+//     document.querySelector(".detail-container").style.backgroundImage = `url('${selectedFilm.background_image}')`;
+//     document.getElementById("poster").src = selectedFilm.poster_image;
+//     document.getElementById("title").textContent = selectedFilm.title;
+//     document.getElementById("description").textContent = selectedFilm.description || '';
+//     document.getElementById("releaseDate").textContent = `Release Date: ${selectedFilm.release_date}`;
+//     document.getElementById("rating").textContent = `Rating: ${selectedFilm.rating} stars`;
+//     document.getElementById("duration").textContent = `Duration: ${selectedFilm.duration} minutes`;
 
 
-  } catch (err) {
-    console.error("Failed to load film details", err);
-  }
-})();
+//   } catch (err) {
+//     console.error("Failed to load film details", err);
+//   }
+// })();
 // document.getElementById("logoutBtn").onclick = () => {
 //   localStorage.clear();
 //   window.location.href = "login.html";
